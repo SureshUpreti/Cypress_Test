a7c6c4ba0825a6e4fc8e92fb82e5b64930d12baf
SureshUpreti <ricardosuresh@gmail.com>
2022-03-10 14:36:01 +0545

    this is first commit for the cypress test automation

---
+++ cypress/integration/example/test1.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', function(){
+
+    it('My First Test Case', function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        cy.get('.product:visible').should('have.length', 4)
+
+        //parent-child chaining(able to get only the required fields what we are lookin for instead of getting the fields from entire page )
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+    })
+    
+})
