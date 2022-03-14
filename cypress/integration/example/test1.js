5febb1b4b6c7290ff9931a39114bca45c3a92163
SureshUpreti <ricardosuresh@gmail.com>
2022-03-14 15:57:59 +0545

    Understanding the concept of each keyword in cypress

--- cypress/integration/example/test1.js
+++ cypress/integration/example/test1.js
@@ -11,6 +11,15 @@ describe('My First Test Suite', function(){
         //parent-child chaining(able to get only the required fields what we are lookin for instead of getting the fields from entire page )
         cy.get('.products').find('.product').should('have.length', 4)
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+        
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews')){
+            cy.wrap($el).find('button').click()
+            }
+        })
+
+        
     })
     
 })
